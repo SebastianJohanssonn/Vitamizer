dc18d4466972b052a1488b8b0c9e0b07e7e503e0
Sebastian Johansson <sebben.97@hotmail.com>
2020-04-08 11:55:10 +0200

    added card and container

---
+++ frontend/src/components/vitaminCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VitaminCard',
+    props: {
+        id: Number,
+        name: String,
+        info: String,
+        sources: Array
+    }
+}
+</script>
---
+++ frontend/src/components/vitaminContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <VitaminCard/>
+    </div>
+</template>
+
+<script>
+import VitaminCard from './vitaminCard.vue'
+export default {
+    name: 'VitaminContainer',
+    components: {
+        VitaminCard
+    },
+    props: {
+        vitamins: null
+    }
+}
+</script>
