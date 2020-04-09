055bfe20c4de008b80e3212cb0fef3216f97bc7a
Sebastian Johansson <sebben.97@hotmail.com>
2020-04-09 11:01:13 +0200

    made jumbotron for vitamins that you want to read more about

---
+++ frontend/src/components/vitaminJumbotron.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="jumbotron">
+        <h1 class="display-4">{{name}}</h1>
+        <p class="lead">{{summary}}</p>
+        <hr class="my-4">
+        <p>{{info}}</p>
+        <hr class="my-4">
+        <h3 class="text-left">Källor för denna vitamin:</h3>
+        <p class="lead text-left">
+            <ul>
+                <li v-for="(source, index) in sources" :key="index">{{source}}</li>
+            </ul>
+        </p>
+        <p class="lead">
+            <button class="btn btn-danger btn-lg" type="button" v-on:click="close">close</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "VitaminJumbotron",
+    props:{
+        name: String,
+        info: String,
+        sources: String,
+        id: Number,
+        summary: String
+    },
+    methods: {
+        close: function(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
