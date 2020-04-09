4e339a79e1e3f30dafba2b0b6d0a1e5439f285a9
Sebastian Johansson <sebben.97@hotmail.com>
2020-04-09 11:01:42 +0200

    small changes, all done

--- frontend/src/components/vitaminCard.vue
+++ frontend/src/components/vitaminCard.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-
-    </div>
+        <div class="card m-3">
+            <div class="card-body">
+                <h5 class="card-title">{{name}}</h5>
+                <p class="card-text">{{summary}}</p>
+                <p class="card-text" v-on:click="readMore">Läs mer</p>
+            </div>
+        </div>
 </template>
 
 <script>
@@ -10,8 +14,12 @@ export default {
     props: {
         id: Number,
         name: String,
-        info: String,
-        sources: Array
+        summary: String
+    },
+    methods: {
+        readMore: function(){
+            this.$emit('idClicked', this.id)
+        }
     }
 }
 </script>
