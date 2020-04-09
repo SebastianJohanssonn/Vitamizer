e078bf1717ef95f554dc22e03edeb07c9f1912fc
Sebastian Johansson <sebben.97@hotmail.com>
2020-04-09 09:51:20 +0200

    made a frame

---
+++ frontend/src/components/frame.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-100">
+    <div class="container-fluid bg-success text-center py-2"><h1 class="text-light">Vitamizer</h1></div>
+    <VitaminContainer/>
+  </div>
+</template>
+
+<script>
+import VitaminContainer from './vitaminContainer'
+
+export default {
+  name: 'Frame',
+  components: {
+    VitaminContainer
+  }
+}
+</script>
