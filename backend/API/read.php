bbd28ffa55995f1f9f5bba11f4f7e8cf1c73c59d
Sebastian Johansson <sebben.97@hotmail.com>
2020-04-09 11:00:10 +0200

    removed uncessary post

--- backend/API/read.php
+++ backend/API/read.php
@@ -2,24 +2,24 @@
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
-    $_POST = json_decode(file_get_contents("php://input"),true);
+    $data = json_decode(file_get_contents("php://input"),true);
     include '../classes/vitamin.php';
 
     if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        if(isset($_GET['id'])){
+            try{
+                $vitamin = new Vitamin('../data/vitamins.json');
+                $result = $vitamin->getOneVitamin($_GET['id']);
+                echo json_encode($result);
+                exit;
+            }catch(Exception $error){
+                echo json_encode($error);
+            }
+        }
         try{
             $vitamins = new Vitamin('../data/vitamins.json');
             $result = $vitamins->getAllVitamins();
-            echo json_encode($vitamins);
-            exit;
-        }catch(Exception $error){
-            echo json_encode($error);
-        }
-    }
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        try{
-            $vitamin = new Vitamin('../data/vitamins.json');
-            $result = $vitamin->getOneVitamin($_POST['id']);
-            echo json_encode($vitamin);
+            echo json_encode($result);
             exit;
         }catch(Exception $error){
             echo json_encode($error);
