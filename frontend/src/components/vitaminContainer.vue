e4f73be05f4f203d4bdf238270231cf029f0d9da
Sebastian Johansson <sebben.97@hotmail.com>
2020-04-09 09:51:12 +0200

    added vitamminContainer

--- frontend/src/components/vitaminContainer.vue
+++ frontend/src/components/vitaminContainer.vue
@@ -1,18 +1,65 @@
 <template>
-    <div>
-        <VitaminCard/>
-    </div>
+    <div class="container">
+        <VitaminCard v-for="(vitamin, index) in vitamins" :key="index"
+            v-bind:id = "vitamin.id"
+            v-bind:name = "vitamin.name"
+            v-bind:info = "vitamin.info"
+            v-bind:sources = "vitamin.sources"
+            v-bind:summary = "vitamin.summary"
+            @idClicked = "handleClick"
+        />
+    </div> 
 </template>
 
 <script>
 import VitaminCard from './vitaminCard.vue'
+import axios from 'axios'
+
 export default {
     name: 'VitaminContainer',
     components: {
         VitaminCard
     },
-    props: {
-        vitamins: null
-    }
+    data(){
+        return{
+            vitamins: undefined,
+            isClicked: false,
+            currentVitamin: undefined,
+            currentVitaminId: undefined
+        }
+    },
+    created(){
+        axios.get('http://localhost:80/Vitamizer/backend/API/read.php')
+        .then(response => {
+        this.vitamins = response.data.JSON.content
+        })
+        .catch(e =>{
+        console.error(e)
+        })
+    },
+    methods: {
+        getOne: function(){
+            axios.post('http://localhost:80/Vitamizer/backend/API/read.php', {
+                id: this.currentVitaminId
+            })
+            .then(response => {
+                this.currentVitamin = response.data.JSON
+                console.log(this.currentVitamin)
+            }).catch(e => {
+                console.error(e)
+            })
+        },
+        handleClick: function(value){
+            this.currentVitaminId = value
+            console.log(value)
+        }
+
+  }
 }
 </script>
+
+<style>
+    .d-flex {
+        height: 100%!important;
+    }
+</style>
